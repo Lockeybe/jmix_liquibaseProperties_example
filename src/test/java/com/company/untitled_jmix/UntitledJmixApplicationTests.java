8a42037359ae9cd899a769caa6275dd3f8cee741
ivanov.v <ivanov.v@ct-sg.ru>
2024-08-29 17:08:40 +0300

    first commit

---
+++ src/test/java/com/company/untitled_jmix/UntitledJmixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.untitled_jmix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntitledJmixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
